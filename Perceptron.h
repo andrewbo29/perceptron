cdac537c133842ad1880b87bd0da1b428cffac22
andrewbo29 <andrewbo29@yandex.ru>
2016-01-19 13:32:23 +0300

    Add class wrapper

--- Perceptron.h
+++ Perceptron.h
@@ -1,11 +1,20 @@
 #include <vector>
 using namespace std;
 
-struct Perceptron
+class Perceptron
 {
+public:
+    Perceptron() = default;
+    Perceptron(const vector<vector<double>> &data);
+    vector<double> *get_weights();
+    void train(vector<vector<double>> &data, vector<int> &labels, int iter_number);
+    void prediction(vector<vector<double>> &data, vector<int> &labels);
+private:
     std::vector<double> weights;
 };
 
-void set_misclass(const vector<vector<double>> *data, const vector<int> *labels, const Perceptron *perc,
-                  vector<vector<double>> &misClass, vector<int> &misClassLabels,
-                  vector<vector<double>> &trueClass, vector<int> &trueClassLabels);
+void setMisclass(const vector<vector<double>> *data, const vector<int> *labels, Perceptron *perc,
+                 vector<vector<double>> &misClass, vector<int> &misClassLabels,
+                 vector<vector<double>> &trueClass, vector<int> &trueClassLabels);
+
+void enterData (vector<vector<double>> &data, vector<int> &labels);
--- main.cpp
+++ main.cpp
@@ -11,10 +11,54 @@ using namespace std;
 
 int main()
 {
-    Perceptron perc;
     vector<vector<double>> data;
     vector<int> labels;
 
+    enterData(data, labels);
+
+    Perceptron perc = Perceptron(data);
+
+    int iter_number = 5;
+
+    perc.train(data, labels, iter_number);
+
+    perc.prediction(data, labels);
+
+    return 0;
+}
+
+void setMisclass(const vector<vector<double>> *data, const vector<int> *labels, Perceptron *perc,
+                 vector<vector<double>> &misClass, vector<int> &misClassLabels,
+                 vector<vector<double>> &trueClass, vector<int> &trueClassLabels) {
+    misClass.clear();
+    misClassLabels.clear();
+    trueClass.clear();
+    trueClassLabels.clear();
+
+    vector<double> *percWeights = perc->get_weights();
+
+    for (decltype(data->size()) i = 0; i < data->size(); ++i) {
+        vector<double> data_elem = (*data)[i];
+        double sum = 0;
+        for (decltype(data_elem.size()) j = 0; j < data_elem.size(); ++j) {
+            sum += (*percWeights)[j] * data_elem[j];
+        }
+        int pred = (sum > 0) ? 1 : -1;
+
+        if (pred != (*labels)[i]) {
+            misClass.push_back(data_elem);
+            misClassLabels.push_back((*labels)[i]);
+        }
+        else {
+            trueClass.push_back(data_elem);
+            trueClassLabels.push_back((*labels)[i]);
+        }
+    }
+
+    return;
+}
+
+void enterData (vector<vector<double>> &data, vector<int> &labels) {
     char c = 'y';
 
     while (c == 'y') {
@@ -46,56 +90,69 @@ int main()
         }
     }
 
+    return;
+}
+
+Perceptron::Perceptron(const vector<vector<double>> &data) {
     for (decltype(data[0].size()) i = 0; i < data[0].size(); ++i) {
         double init_weight = static_cast<double>(rand()) / (RAND_MAX);
-        perc.weights.push_back(init_weight);
+        weights.push_back(init_weight);
     }
 
     cout << "Weights" << endl;
-    for (auto w : perc.weights) {
+    for (auto w : weights) {
         cout << w << "\t";
     }
     cout << endl;
+}
 
-    vector<vector<double>> misclass;
-    vector<int> misclass_labels;
+void Perceptron::train(vector<vector<double>> &data, vector<int> &labels, int iter_number) {
 
-    vector<vector<double>> trueclass;
-    vector<int> trueclass_labels;
+    vector<vector<double>> misClass;
+    vector<int> misClassLabels;
 
-    set_misclass(&data, &labels, &perc, misclass, misclass_labels, trueclass, trueclass_labels);
+    vector<vector<double>> trueClass;
+    vector<int> trueClassLabels;
 
-    int iter_number = 5;
+    setMisclass(&data, &labels, this, misClass, misClassLabels, trueClass, trueClassLabels);
 
     int done_iter = 0;
-    while (!misclass.empty() && done_iter < iter_number) {
+    while (!misClass.empty() && done_iter < iter_number) {
         cout << "Learning iteration number " << done_iter << endl;
         ++done_iter;
 
-        int max_ind = misclass.size();
+        int max_ind = misClass.size();
         int rand_ind = rand() % max_ind;
-        vector<double> rand_elem = misclass[rand_ind];
+        vector<double> rand_elem = misClass[rand_ind];
 
-        //cout << rand_elem.size() << "\t" << perc.weights.size() << endl;
+        //cout << rand_elem.size() << "\t" << weights.size() << endl;
 
-        for (decltype(perc.weights.size()) k = 0; k < perc.weights.size(); ++k) {
-            perc.weights[k] += misclass_labels[rand_ind] * rand_elem[k];
+        for (decltype(weights.size()) k = 0; k < weights.size(); ++k) {
+            weights[k] += misClassLabels[rand_ind] * rand_elem[k];
         }
 
-        set_misclass(&data, &labels, &perc, misclass, misclass_labels, trueclass, trueclass_labels);
+        setMisclass(&data, &labels, this, misClass, misClassLabels, trueClass, trueClassLabels);
 
         cout << "Weights" << endl;
-        for (auto w : perc.weights) {
+        for (auto w : weights) {
             cout << w << "\t";
         }
         cout << endl;
     }
 
+    return;
+}
+
+vector<double> *Perceptron::get_weights() {
+    return &weights;
+}
+
+void Perceptron::prediction(vector<vector<double>> &data, vector<int> &labels) {
     for (decltype(data.size()) i = 0; i < data.size(); ++i) {
         vector<double> data_elem = data[i];
         double sum = 0;
         for (decltype(data_elem.size()) j = 0; j < data_elem.size(); ++j) {
-            sum += perc.weights[j] * data_elem[j];
+            sum += weights[j] * data_elem[j];
         }
         int pred = (sum > 0) ? 1 : -1;
 
@@ -111,33 +168,5 @@ int main()
         cout << c1 << endl;
     }
 
-    return 0;
-}
-
-
-void set_misclass(const vector<vector<double>> *data, const vector<int> *labels, const Perceptron *perc,
-                  vector<vector<double>> &misClass, vector<int> &misClassLabels,
-                  vector<vector<double>> &trueClass, vector<int> &trueClassLabels) {
-    misClass.clear();
-    misClassLabels.clear();
-    trueClass.clear();
-    trueClassLabels.clear();
-
-    for (decltype(data->size()) i = 0; i < data->size(); ++i) {
-        vector<double> data_elem = (*data)[i];
-        double sum = 0;
-        for (decltype(data_elem.size()) j = 0; j < data_elem.size(); ++j) {
-            sum += perc->weights[j] * data_elem[j];
-        }
-        int pred = (sum > 0) ? 1 : -1;
-
-        if (pred != (*labels)[i]) {
-            misClass.push_back(data_elem);
-            misClassLabels.push_back((*labels)[i]);
-        }
-        else {
-            trueClass.push_back(data_elem);
-            trueClassLabels.push_back((*labels)[i]);
-        }
-    }
+    return;
 }
